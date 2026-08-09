4507bd3e524d4ba6160166bf034efa21b78173da
vijayswarnkar21 <vijay.swarnkar.vdn@gmail.com>
2026-08-09 22:30:59 +0530

    split pdf content

--- JECRCChatBot.py
+++ JECRCChatBot.py
@@ -1,7 +1,22 @@
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
 load_dotenv()
+
+chunks = []
 
 loader = PyPDFLoader("JECRC_Overview_Embedding_Ready.pdf")
 docs = loader.load()
-print(docs[0])
+
+for doc in docs:
+    chunk = text_splitter.split_text(doc.page_content)
+    chunks.append(chunk)  
+
+
+
+
+
