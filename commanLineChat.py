59e61b9300092923eac11cc27f0c9f892f409b51
vijayswarnkar21 <vijay.swarnkar.vdn@gmail.com>
2026-08-09 21:37:36 +0530

    chat app with a list of message

--- commanLineChat.py
+++ commanLineChat.py
@@ -3,12 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+chats = []
+
 model = ChatAnthropic(model="claude-haiku-4-5")
 
 while True:
     user = input("Enter Your Message: - ")
     message = user.strip()
+    chats.append(message)
     if message.lower() == "exit":
         break
-    result = model.invoke(message)
+    result = model.invoke(chats)
+    chats.append(result.content)
     print(result.content)    
